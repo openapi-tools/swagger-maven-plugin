74cc79b2dc85dba20aef763aa22c64dab4694d0e
Carsten Wickner <carsten@torque-its.com>
2024-01-02 21:36:42 +0100

    feat: replace reflections lib with classgraph

--- src/main/java/io/openapitools/swagger/JaxRSScanner.java
+++ src/main/java/io/openapitools/swagger/JaxRSScanner.java
@@ -2,6 +2,7 @@ package io.openapitools.swagger;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,12 +11,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
 
 import org.apache.maven.plugin.logging.Log;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ConfigurationBuilder;
 
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 /**
@@ -33,46 +33,41 @@ class JaxRSScanner {
     public JaxRSScanner(Log log, Set<String> resourcePackages, Boolean useResourcePackagesChildren) {
         this.log = log;
         this.resourcePackages = resourcePackages == null ? Collections.emptySet() : new HashSet<>(resourcePackages);
-        this.useResourcePackagesChildren = useResourcePackagesChildren != null && useResourcePackagesChildren;
+        this.useResourcePackagesChildren = Boolean.TRUE.equals(useResourcePackagesChildren);
     }
 
     Application applicationInstance() {
-        ConfigurationBuilder config = ConfigurationBuilder
-                .build(resourcePackages)
-                .setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
-        Reflections reflections = new Reflections(config);
-        Set<Class<? extends Application>> applicationClasses = reflections.getSubTypesOf(Application.class)
-                .stream()
-                .filter(this::filterClassByResourcePackages)
-                .collect(Collectors.toSet());
-        if (applicationClasses.isEmpty()) {
-            return null;
+        ClassGraph classGraph = new ClassGraph().enableClassInfo();
+        try (ScanResult scanResult = classGraph.scan()) {
+            List<ClassInfo> applicationClasses = scanResult.getSubclasses(Application.class.getName()).stream()
+                    .filter(this::filterClassByResourcePackages)
+                    .collect(Collectors.toList());
+            if (applicationClasses.size() == 1) {
+                return ClassUtils.createInstance(applicationClasses.get(0).loadClass(Application.class));
+            }
+            if (applicationClasses.size() > 1) {
+                log.warn("More than one javax.ws.rs.core.Application classes found on the classpath, skipping");
+            }
         }
-        if (applicationClasses.size() > 1) {
-            log.warn("More than one javax.ws.rs.core.Application classes found on the classpath, skipping");
-            return null;
-        }
-        return ClassUtils.createInstance(applicationClasses.iterator().next());
+        return null;
     }
 
     Set<Class<?>> classes() {
-        ConfigurationBuilder config = ConfigurationBuilder
-                .build(resourcePackages)
-                .setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
-        Reflections reflections = new Reflections(config);
-        Stream<Class<?>> apiClasses = reflections.getTypesAnnotatedWith(Path.class)
-                .stream()
-                .filter(this::filterClassByResourcePackages);
-        Stream<Class<?>> defClasses = reflections.getTypesAnnotatedWith(OpenAPIDefinition.class)
-                .stream()
-                .filter(this::filterClassByResourcePackages);
-        return Stream.concat(apiClasses, defClasses).collect(Collectors.toSet());
+        ClassGraph classGraph = new ClassGraph().enableClassInfo().enableAnnotationInfo();
+        try (ScanResult scanResult = classGraph.scan()) {
+            ClassInfoList apiClasses = scanResult.getClassesWithAnnotation(Path.class.getName());
+            ClassInfoList defClasses = scanResult.getClassesWithAnnotation(OpenAPIDefinition.class.getName());
+            return Stream.concat(apiClasses.stream(), defClasses.stream())
+                    .filter(this::filterClassByResourcePackages)
+                    .map(ClassInfo::loadClass)
+                    .collect(Collectors.toSet());
+        }
     }
 
-    private boolean filterClassByResourcePackages(Class<?> cls) {
+    private boolean filterClassByResourcePackages(ClassInfo cls) {
         return resourcePackages.isEmpty()
-                || resourcePackages.contains(cls.getPackage().getName())
-                || (useResourcePackagesChildren && resourcePackages.stream().anyMatch(p -> cls.getPackage().getName().startsWith(p)));
+                || resourcePackages.contains(cls.getPackageName())
+                || (useResourcePackagesChildren && resourcePackages.stream().anyMatch(p -> cls.getPackageName().startsWith(p)));
     }
 
 }
